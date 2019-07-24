40656914aafad02514307fa4d5ea9df5ad02859e
bakapear <proxybomb@gmail.com>
2019-07-24 04:19:31 +0200

    new stuff

--- lib/despair.d.ts
+++ lib/despair.d.ts
@@ -1,4 +1,4 @@
-import { PathLike, WriteFileOptions, Stats } from 'fs'
+import { MakeDirectoryOptions } from "fs";
 
 export function distinct(array: any[]): any[]
 export function hms(milliseconds: number): string
@@ -6,13 +6,16 @@ export function match(array: Object[], property: PropertyKey, query: string): Ob
 export function shuffle(array: any[]): any[]
 export function wait(milliseconds: number): Promise<void>
 export function format(string: string, ...arguments: any): string
-export function color(flags: string, str: string): string
+export function color(str: string): string
 export function isURL(string: string, lazy: boolean): boolean
 export function escapeRegex(string: string): string
-
-export function write(path: PathLike, data: any, options?: WriteFileOptions): void
+export function write(path: PathLike, data: any, options?: WriteFileOptions | MakeDirectoryOptions): void
 export function read(path: PathLike, options?: { encoding?: null; flag?: string }): Buffer | string[]
 export function exists(path: PathLike): boolean
 export function stat(path: PathLike): Stats
-
-export function fetch(url: URL, options?: Object): Promise<any>
+export function fetch(url: got.GotUrl, options?: got.GotJSONOptions): got.GotPromise<any>
+export function html(html: string | Buffer, options?: CheerioOptionsInterface): CheerioStatic
+export function rnd(min: number, max: number): number
+export function encode(str: string, type: string): string
+export function decode(str: string, type: string): string
+export function test(fn: Function | any, any: any, full?: boolean): boolean
---
+++ test/basic.js
@@ -0,0 +1,46 @@
+let dp = require('../lib/despair')
+
+dp.test(
+  dp.distinct(['cheese', 'burger', 1, 2, 3, 2, 'cheese']),
+  ['cheese', 'burger', 1, 2, 3]
+)
+
+dp.test(
+  dp.hms(843483250),
+  '234:18:03'
+)
+
+dp.test(
+  dp.match([{ name: 'cheese' }, { name: 'cloth' }, { name: 'cheese cake' }], 'name', 'che'),
+  [{ name: 'cheese', score: 44 }, { name: 'cheese cake', score: 39 }]
+)
+
+dp.test(
+  dp.format('To {0} is also to {0} how to {1}.', 'forget', 'evolve'),
+  'To forget is also to forget how to evolve.'
+)
+
+dp.test(
+  dp.isURL('example.com', true),
+  true
+)
+
+dp.test(
+  dp.escapeRegex('What§*+34)(F4+'),
+  'What§\\*\\+34\\)\\(F4\\+'
+)
+
+dp.test(
+  dp.encode('The ability to think, the ability to continue thinking.', 'base64'),
+  'VGhlIGFiaWxpdHkgdG8gdGhpbmssIHRoZSBhYmlsaXR5IHRvIGNvbnRpbnVlIHRoaW5raW5nLg=='
+)
+
+dp.test(
+  dp.decode('Rm9yZ2V0PyBJZGlvdGljLg==', 'base64'),
+  'Forget? Idiotic.'
+)
+
+dp.test(
+  dp.decode('&lt;html&gt;stuff&lt;/html&gt;', 'html'),
+  '<html>stuff</html>'
+)
